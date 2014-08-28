4095d6ffa2a6f6826edd6fc00dd16c6444978b29
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-08-28 22:04:17 +1000

    Remove old comments

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,13 +1,6 @@
-//many items left from tutorial
-//http://www.anupshinde.com/posts/how-to-create-nodejs-npm-package/
 "use strict"
-//var fs = require('fs');
-//var schema = require('validate');
-//var _ = require('underscore');
-//var S = require('string');
 var utils = require('./utils');
 var getEntry = utils.getEntry;
 var getRecord = utils.getRecord;
-
 
 exports.getRecord = getRecord;
