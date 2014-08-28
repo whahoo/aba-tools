5cf431a04f086f59eea671c599fbdc7fd3f804f9
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-08-28 13:38:43 +1000

    Tutorial Part 2

--- uppercaseme.js
+++ uppercaseme.js
@@ -1,14 +1,18 @@
 //http://www.anupshinde.com/posts/how-to-create-nodejs-npm-package/
 //uppercaseme.js
-
 "use strict"
 var fs = require('fs');
-var myFile = "myfile.txt";
 
-if ( fs.existsSync( myFile ) ) {
-  var content = fs.readFileSync(myFile, 'utf8');
-  fs.writeFileSync( myFile, content.toUpperCase() );
-  console.log( "Done" );
+if ( process.argv.length > 2 ) {
+  var myFile = process.argv[2];
+
+  if ( fs.existsSync( myFile ) ) {
+    var content = fs.readFileSync(myFile, 'utf8');
+    fs.writeFileSync( myFile, content.toUpperCase() );
+    console.log( "Done" );
+  } else {
+    console.log( "File does not exist - " + myFile );
+  }
 } else {
-  console.log( "File does not exist - " + myFile );
+  console.log( "ERROR: Pass on a file name/path" );
 }
