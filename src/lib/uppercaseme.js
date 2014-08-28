94a6c310c114230a85a0dfa6a3053cbb98292a69
LB (Ben) Johnston <lb@doubleampersand.com.au>
2014-08-28 13:50:58 +1000

    Tutorial Part 4

--- src/lib/uppercaseme.js
+++ src/lib/uppercaseme.js
@@ -1,18 +1,21 @@
-//http://www.anupshinde.com/posts/how-to-create-nodejs-npm-package/
 //uppercaseme.js
+//http://www.anupshinde.com/posts/how-to-create-nodejs-npm-package/
 "use strict"
 var fs = require('fs');
 
-if ( process.argv.length > 2 ) {
-  var myFile = process.argv[2];
-
-  if ( fs.existsSync( myFile ) ) {
-    var content = fs.readFileSync(myFile, 'utf8');
-    fs.writeFileSync( myFile, content.toUpperCase() );
-    console.log( "Done" );
+function convertThis() {
+  if ( process.argv.length > 2 ) {
+    var myFile = process.argv[2];
+    if ( fs.existsSync( myFile ) ) {
+      var content = fs.readFileSync(myFile, 'utf8');
+      fs.writeFileSync( myFile, content.toUpperCase() );
+      console.log( "Done" );
+    } else {
+      console.log( "File does not exist - " + myFile );
+    }
   } else {
-    console.log( "File does not exist - " + myFile );
+    console.log( "ERROR: Pass on a file name/path" );
   }
-} else {
-  console.log( "ERROR: Pass on a file name/path" );
 }
+
+exports.convert = convertThis;
