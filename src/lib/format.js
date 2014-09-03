77909efbf37391d7207a277d016f45f74278b97b
LB (Ben) Johnston <github@lb.ee>
2014-09-03 21:08:37 +1000

    Add Indicator formatter

--- src/lib/format.js
+++ src/lib/format.js
@@ -6,12 +6,16 @@ var moment = require('moment');
 var as = {};
 
 as.v = function formatAsValue(value) {
-  //console.log('this', this, 'value', value);
   var self = this;//will be the column definition
-  //console.log('this',this, 'value',value);
-  if (value === undefined) {
-    console.log('value is undefined', value, self);
+  if (! value) {
+    throw new Error('value is undefined');
   }
+  return value.toString().trim().substr(0, self.size);
+};
+
+as.indicator = function formatAsValue(value) {
+  var self = this;
+  var value = value || '';
   return value.toString().trim().substr(0, self.size);
 };
 
