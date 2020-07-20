963fd079a52d58b29254df23a2d1ab9406cb7112
Simon Harrison <simon@evo2.com>
2020-07-20 15:46:27 +1000

    update for new hapi/joi updates

--- src/lib/config.js
+++ src/lib/config.js
@@ -15,13 +15,13 @@ var config = {};
 config.joi = {
   abortEarly: false,
   stripUnknown: true,
-  convert: true
+  convert: true,
 };
 
 config.recordTypes = {
   descriptive: { id: "0" },
   detail: { id: "1" },
-  total: { id: "7" }
+  total: { id: "7" },
 };
 
 config.recordTypeNames = Object.keys(config.recordTypes);
@@ -32,7 +32,7 @@ config.recordTypeNames = Object.keys(config.recordTypes);
 var firstColumn = {
   size: 1,
   key: "typeId",
-  format: as.v
+  format: as.v,
 };
 //bsb stands for bank state branch. Standard format is ###-###
 //http://en.wikipedia.org/wiki/Bank_State_Branch
@@ -47,35 +47,22 @@ config.column.schema = Joi.object()
   .required()
   .keys({
     //max column size 40, no columns are larger in the spec
-    size: Joi.number()
-      .min(1)
-      .max(40)
-      .required(),
+    size: Joi.number().min(1).max(40).required(),
     //some columns are just fillers of space or zeros, this boolan does not g
     //used currently, potential use in future
-    blank: Joi.boolean()
-      .optional()
-      .default(false),
+    blank: Joi.boolean().optional().default(false),
     //key will be the key for the record object passed in
-    key: Joi.string()
-      .token()
-      .optional(),
+    key: Joi.string().token().optional(),
     //some columns require space fills, others require zero fil
     //eg. account number 1235 would become 000001245
-    fill: Joi.any()
-      .valid([" ", "0"])
-      .optional()
-      .default(" "),
+    fill: Joi.any().valid(" ", "0").optional().default(" "),
     //the spec uses the term justify, this determines the padding direction
     //eg. right justify padding with zeroes would make 33456 into 334560000000
-    justify: Joi.string()
-      .valid(["left", "right"])
-      .optional()
-      .default("left"),
+    justify: Joi.string().valid("left", "right").optional().default("left"),
     //function provided for the column to parse the entry, this is mostly for
     //formatting, in some cases it will provide additional transformation
     //task: I could not get the .default(as.v) to work, need to look at Joi
-    format: Joi.func().required()
+    format: Joi.func().required(),
   });
 
 //Descriptive Record
@@ -91,17 +78,14 @@ config.recordTypes.descriptive.columns = [
   { size: 6, key: "userId", fill: "0", justify: "right", format: as.v },
   { size: 12, key: "description", fill: " ", justify: "left", format: as.v },
   { size: 6, key: "date", format: as.date },
-  { size: 40, blank: true, format: as.v }
+  { size: 40, blank: true, format: as.v },
 ];
 
 config.recordTypes.descriptive.schema = Joi.object()
   .required()
   .keys({
     //this seems to be just 1 most of the time
-    sequence: Joi.number()
-      .min(1)
-      .max(99)
-      .required(),
+    sequence: Joi.number().min(1).max(99).required(),
     //this needs to be a three letter bank code eg. BQL or WBC
     //readme.md or http://www.thebsbnumbers.com/ - List of Financial Institution Codes
     bank: Joi.string()
@@ -111,28 +95,13 @@ config.recordTypes.descriptive.schema = Joi.object()
       .regex(/^[A-Z]{3}(?:List)?$/)
       .required(),
     //usually the company name
-    userName: Joi.string()
-      .allow(" ")
-      .min(1)
-      .max(26)
-      .trim()
-      .required(),
+    userName: Joi.string().allow(" ").min(1).max(26).trim().required(),
     //not 100% sure where this number comes from
-    userId: Joi.number()
-      .min(1)
-      .max(999999)
-      .required(),
+    userId: Joi.number().min(1).max(999999).required(),
     //name or description of the transaction as a whole eg. payroll or payments
-    description: Joi.string()
-      .min(1)
-      .max(12)
-      .alphanum()
-      .trim()
-      .required(),
+    description: Joi.string().min(1).max(12).alphanum().trim().required(),
     //javascript date required, saves the user from having to format themselves
-    date: Joi.date()
-      .optional()
-      .default(new Date())
+    date: Joi.date().optional().default(new Date()),
   });
 
 //Detail Record
@@ -155,7 +124,7 @@ config.recordTypes.detail.columns = [
   { size: 7, key: "fromBsb", format: as.v },
   { size: 9, key: "fromAcc", fill: " ", justify: "right", format: as.v },
   { size: 16, key: "fromName", fill: " ", justify: "left", format: as.v },
-  { size: 8, key: "tax", fill: "0", justify: "right", format: as.amount }
+  { size: 8, key: "tax", fill: "0", justify: "right", format: as.amount },
 ];
 
 config.recordTypes.detail.schema = Joi.object()
@@ -164,48 +133,30 @@ config.recordTypes.detail.schema = Joi.object()
     //originator's name will appear on other person's statement
     fromName: Joi.string().required(),
     //BSB for account to transfer from
-    fromBsb: Joi.string()
-      .regex(bsbRegex)
-      .required(),
+    fromBsb: Joi.string().regex(bsbRegex).required(),
     //account number for account to transfer from
-    fromAcc: Joi.string()
-      .regex(accountNumberRegex)
-      .required(),
+    fromAcc: Joi.string().regex(accountNumberRegex).required(),
     //receiver account name, usually first name & last name
-    toName: Joi.string()
-      .min(1)
-      .max(32)
-      .required(),
+    toName: Joi.string().min(1).max(32).required(),
     //reference that will appear on external / receiver statement
     toRef: Joi.string()
       .regex(/^[1-9A-Za-z]{1}[0-9A-Za-z ]{0,17}$/)
       .required(),
     //BSB for account to transfer to
-    toBsb: Joi.string()
-      .regex(bsbRegex)
-      .required(),
+    toBsb: Joi.string().regex(bsbRegex).required(),
     //account number for account to transfer to
     toAcc: Joi.required(),
     //See readme for definition, single space or letter for this record's type
-    indicator: Joi.string()
-      .valid(["N", "W", "X", "Y"])
-      .optional(),
+    indicator: Joi.string().valid("N", "W", "X", "Y").optional(),
     //See readme, transaction code usually 53
     transaction: Joi.number()
-      .valid([13, 50, 51, 52, 53, 54, 55, 56, 57])
+      .valid(13, 50, 51, 52, 53, 54, 55, 56, 57)
       .optional()
       .default(53),
     //parse function will make 1103.2321 -> 00000110323 (round, remove decimal, pad)
-    amount: Joi.number()
-      .min(1)
-      .max(99999999)
-      .required(),
+    amount: Joi.number().min(1).max(99999999).required(),
     //smaller amount but treated as amount, is the tax withheld
-    tax: Joi.number()
-      .min(0)
-      .max(999999)
-      .optional()
-      .default(0)
+    tax: Joi.number().min(0).max(999999).optional().default(0),
   });
 
 //File Total Record
@@ -222,40 +173,29 @@ config.recordTypes.total.columns = [
     key: "totalCredit",
     fill: "0",
     justify: "right",
-    format: as.amount
+    format: as.amount,
   },
   {
     size: 10,
     key: "totalDebit",
     fill: "0",
     justify: "right",
-    format: as.amount
+    format: as.amount,
   },
   { size: 24, blank: true, format: as.v },
   { size: 6, key: "count", fill: "0", justify: "right", format: as.v },
-  { size: 40, blank: true, format: as.v }
+  { size: 40, blank: true, format: as.v },
 ];
 
 config.recordTypes.total.schema = Joi.object()
   .required()
   .keys({
     //all totals from the other 'detail' records'
-    totalNet: Joi.number()
-      .min(1)
-      .max(99999999)
-      .required(),
-    totalCredit: Joi.number()
-      .min(0)
-      .max(99999999)
-      .required(),
-    totalDebit: Joi.number()
-      .min(0)
-      .max(99999999)
-      .required(),
+    totalNet: Joi.number().min(1).max(99999999).required(),
+    totalCredit: Joi.number().min(0).max(99999999).required(),
+    totalDebit: Joi.number().min(0).max(99999999).required(),
     //total count of records with type 1 (detail records) - ie. checksum
-    count: Joi.number()
-      .min(1)
-      .required()
+    count: Joi.number().min(1).required(),
   });
 
 if (typeof module !== "undefined") {
