446e88112eb5bb82bacaf8a4253d542949e777a1
Simon Harrison <simon@evo2.com>
2020-07-20 17:08:05 +1000

    updates to allow new hapi/joi to function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,42 +1,41 @@
-"use strict"
+"use strict";
 
-
-if (typeof Meteor === 'undefined' /*import if npm*/) {
+if (typeof Meteor === "undefined" /*import if npm*/) {
   //external
-  var _ = require('underscore');
-  var _s = require('underscore.string');
-  var Joi = require('joi');
+  var _ = require("underscore");
+  var _s = require("underscore.string");
+  var Joi = require("joi");
 
   //internal
-  var config = require('./config');
+  var config = require("./config");
   var recordTypes = config.recordTypes;
   var recordTypeNames = config.recordTypeNames;
 }
 
 var utils = {};
 
-utils.checkType = function checkType (type, excludeTotal) {
+utils.checkType = function checkType(type, excludeTotal) {
   var type = type || null;
   var excludeTotal = excludeTotal || false;
+  var local_recordTypeNames = recordTypeNames;
   //Type is either; 'descriptive', 'detail', 'file-total'
   //Type defaults to 'detail' as that will be the most common
   if (excludeTotal) {
-    recordTypeNames = _.without(_.clone(recordTypeNames), 'total');
-  };
-  Joi.validate(type,
-    Joi.valid(recordTypeNames).default('detail').optional(),
-    config.joi, function (error, response) {
-      if (error) throw error;
-      type = response;
-    }
-  );
+    local_recordTypeNames = _.without(_.clone(recordTypeNames), "total");
+  }
+  const schema = Joi.any()
+    .valid(...local_recordTypeNames)
+    .default("detail")
+    .optional();
+  const { error, value } = schema.validate(type, config.joi);
+  if (error) throw error;
   return type;
-}
+};
 
 //justifying is the last step, this pads with zeros or spaces
-utils.justifyValue = function justifyValue (value, size, fill, justify) {
+utils.justifyValue = function justifyValue(value, size, fill, justify) {
   var result;
-  if (justify === 'right') {
+  if (justify === "right") {
     result = _s.lpad(value, size, fill);
   } else {
     result = _s.rpad(value, size, fill);
@@ -46,34 +45,33 @@ utils.justifyValue = function justifyValue (value, size, fill, justify) {
 
 //Return a fully parsed entry as a string
 //This processes an entry based on a column object
-utils.getEntry = function getEntry (value, column) {
-  var value, column, result;
+utils.getEntry = function getEntry(in_value, column) {
+  var in_value, column, result;
   //check that the column is valid according to the schema
-  Joi.validate(column, config.column.schema, config.joi, function (error, response) {
-    if (error) {
-      throw error;
-    } else {
-      //this should be a function based on the column.parse
-      value = column['format'](value);
-      result = utils.justifyValue(value, column.size, column.fill, column.justify);
-    }
-  });
+  const { error, value } = config.column.schema.validate(column, config.joi);
+  if (error) throw error;
+  in_value = column["format"](value);
+  result = utils.justifyValue(
+    in_value,
+    column.size,
+    column.fill,
+    column.justify
+  );
   //always return something
   //task: maybe validate this is a string longer than 0 characters
   return result;
 };
 
-
 //For an object of values and an array of columns process each column
-utils.getColumns = function getColumns (values, columns) {
+utils.getColumns = function getColumns(values, columns) {
   var values, columns, result;
-  result = '';
-  if (columns.length < 8 ) {
-    throw new Error('There should be at least 8 columns', columns);
+  result = "";
+  if (columns.length < 8) {
+    throw new Error("There should be at least 8 columns", columns);
   } else {
-    _.each(columns, function(column) {
+    _.each(columns, function (column) {
       //Set default value to empty string or get the value from options
-      var value = '';
+      var value = "";
       if (column.key) {
         value = values[column.key];
       }
@@ -87,32 +85,27 @@ utils.getColumns = function getColumns (values, columns) {
 
 //returns a record, which is essentially a row in the final ABA file
 //each record has a type and that type determines the columns and validation
-utils.getRecord = function getRecord (type, values) {
+utils.getRecord = function getRecord(type, values) {
   var result, type, values, columns;
   type = utils.checkType(type);
   columns = recordTypes[type].columns;
   //validate the value object provided based on the schema for that record type
-  Joi.validate(values, recordTypes[type].schema, config.joi,
-    function (error, response) {
-      if (error) {
-        throw error
-      } else {
-        //add tye type & typeId to the values sent to getColmns
-        var values = _.extend({ type: type, typeId: recordTypes[type].id }, response);
-        //console.log('value after processing ', values);
-        result = utils.getColumns(values, columns);
-      }
-    }
+  const { error, value } = recordTypes[type].schema.validate(
+    values,
+    config.joi
   );
+  if (error) throw error;
+  var values = _.extend({ type: type, typeId: recordTypes[type].id }, value);
+  result = utils.getColumns(values, columns);
 
-  if (! result.length === 120) {
+  if (!result.length === 120) {
     //task: could use Joi here also
-    throw new Error('Record length must be exactly 120 characters');
+    throw new Error("Record length must be exactly 120 characters");
   }
 
   return result;
-}
+};
 
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = utils;
 }
--- test/tests.js
+++ test/tests.js
@@ -1,81 +1,83 @@
-"use strict"
+"use strict";
 
 //external
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs');
-var _ = require('underscore');
-var Joi = require('joi');
+var assert = require("assert");
+var path = require("path");
+var fs = require("fs");
+var _ = require("underscore");
+var Joi = require("joi");
 
 //internal
-var src = path.join(path.dirname(fs.realpathSync(__filename)), '../src');
-var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../src/lib');
-var config = require(lib + '/config');
+var src = path.join(path.dirname(fs.realpathSync(__filename)), "../src");
+var lib = path.join(path.dirname(fs.realpathSync(__filename)), "../src/lib");
+var config = require(lib + "/config");
 var recordTypes = config.recordTypes;
 var recordTypeNames = config.recordTypeNames;
-var utils = require(lib + '/utils');
+var utils = require(lib + "/utils");
 var getEntry = utils.getEntry;
 var getColumns = utils.getColumns;
 var getRecord = utils.getRecord;
 
-var ABA = require(src + '/main');
+var ABA = require(src + "/main");
 
-
-function testBadInputs (recordType, fieldKey, badInputs, values) {
+function testBadInputs(recordType, fieldKey, badInputs, values) {
   var badInputs = badInputs || [];
-  var fieldKey = fieldKey || 'title not provided';
+  var fieldKey = fieldKey || "title not provided";
   _.each(badInputs, function (badInput) {
     var i = {};
     i[fieldKey] = badInput;
     var badInputs = _.extend(_.clone(values), i);
-    if (badInput === 'not provided') {
+    if (badInput === "not provided") {
       delete badInputs[fieldKey];
     }
     //this regex makes sure that the errir is a ValidationError AND includes the fieldkey name
     //otherwise validataion errors may pass that are caused by something else
-    var errorRegex = new RegExp("^(?=.*\\bValidationError\\b)(?=.*\\b"+fieldKey+"\\b).*$");
-    it("should throw ValidationError if " + fieldKey + " is [ " + badInput + " ]", function () {
-      assert.throws(
-        function () { getRecord(recordType, badInputs) },
-        errorRegex
-      );
-    });
+    var errorRegex = new RegExp(
+      "^(?=.*\\bValidationError\\b)(?=.*\\b" + fieldKey + "\\b).*$"
+    );
+    it(
+      "should throw ValidationError if " +
+        fieldKey +
+        " is [ " +
+        badInput +
+        " ]",
+      function () {
+        assert.throws(function () {
+          getRecord(recordType, badInputs);
+        }, errorRegex);
+      }
+    );
   });
-};
-
+}
 
 // configuration integrity tests
 
-describe('# config', function () {
-  _.each(['descriptive', 'detail', 'total'], function (type) {
-    describe('Config for ' + type + ' Columns', function () {
-
-      it('should get a total of 120 characters', function () {
-        var totalCharsInColumns = _.reduce(recordTypes[type].columns, function (total, column) {
-          return total + column.size;
-        }, 0);
+describe("# config", function () {
+  _.each(["descriptive", "detail", "total"], function (type) {
+    describe("Config for " + type + " Columns", function () {
+      it("should get a total of 120 characters", function () {
+        var totalCharsInColumns = _.reduce(
+          recordTypes[type].columns,
+          function (total, column) {
+            return total + column.size;
+          },
+          0
+        );
         assert.equal(120, totalCharsInColumns);
       });
 
-      it('should validate each column against the column schema', function () {
+      it("should validate each column against the column schema", function () {
         _.each(recordTypes[type].columns, function (column) {
-          Joi.validate(column, config.column.schema, config.joi, function (error, response) {
-            if (error) {
-              throw error;
-              //not sure if this should be done differently in test env.
-            }
-          });
+          const { error } = config.column.schema.validate(column, config.joi);
+          if (error) throw error;
+          //not sure if this should be done differently in test env.
         });
       });
-
-
     });
   });
-
 });
 
-
-describe('# utils', function () {
+describe("# utils", function () {
   var good = {
     descriptive: {},
     detail: {},
@@ -84,21 +86,21 @@ describe('# utils', function () {
 
   good.descriptive.values = {
     sequence: 1,
-    bank: 'WBC',
-    userName: 'TRANSPORT CO',
-    userId: '212459',
-    description: 'Payments',
+    bank: "WBC",
+    userName: "TRANSPORT CO",
+    userId: "212459",
+    description: "Payments",
     date: new Date(),
   };
 
   good.detail.values = {
-    fromName: 'Transport Co',
-    fromBsb: '034-213',
-    fromAcc: '991882',
-    toName: 'Billy Andrews',
-    toRef: 'INV 3217B',
-    toBsb: '117-213',
-    toAcc: '00112228',
+    fromName: "Transport Co",
+    fromBsb: "034-213",
+    fromAcc: "991882",
+    toName: "Billy Andrews",
+    toRef: "INV 3217B",
+    toBsb: "117-213",
+    toAcc: "00112228",
     //indicator: ' ',
     transaction: 50,
     amount: 1189.22,
@@ -108,229 +110,286 @@ describe('# utils', function () {
   good.total.values = {
     totalNet: 3523.32,
     totalCredit: 8271.82,
-    totalDebit: 4748.50,
+    totalDebit: 4748.5,
     count: 12,
   };
 
-  describe('getRecord()', function () {
-
-
+  describe("getRecord()", function () {
     it("should throw ValidationError if type is not 'descriptive', 'detail' or 'file-total'", function () {
-
-      _.each(['foo','#'], function (v) {
-        assert.throws(
-          function () {
-            getRecord(v, good.descriptive.values)
-          },
-          /ValidationError/
-        );
+      _.each(["foo", "#"], function (v) {
+        assert.throws(function () {
+          getRecord(v, good.descriptive.values);
+        }, /ValidationError/);
       });
+    });
+  });
 
+  describe("descriptive record", function () {
+    describe("sequence", function () {
+      testBadInputs(
+        "descriptive",
+        this.title,
+        ["not provided", null, undefined, "a"],
+        good.descriptive.values
+      );
     });
 
-  });
-
-  describe('descriptive record', function () {
-
-      describe('sequence', function () {
-        testBadInputs( 'descriptive', this.title, ['not provided', null, undefined, 'a'], good.descriptive.values);
-      });
-
-      describe('bank', function () {
-        testBadInputs( 'descriptive', this.title, ['not provided', null, undefined, 'a', 'ab', 'abcd', '###', 123, '123'], good.descriptive.values);
-      });
-
-      describe('userName', function () {
-        testBadInputs( 'descriptive', this.title, ['not provided', null, undefined, '',], good.descriptive.values);
-      });
-
-      describe('userId', function () {
-        testBadInputs( 'descriptive', this.title, ['not provided', null, undefined, ''], good.descriptive.values);
-      });
-
-      describe('description', function () {
-        testBadInputs( 'descriptive', this.title, ['not provided', null, undefined, '', 'very long description', 1232344], good.descriptive.values);
-      });
-
-      describe('date', function () {
-        testBadInputs( 'descriptive', this.title, ['a', 'ab', 'abcd', '###', '23/12/2014'], good.descriptive.values);
-      });
-
-  });
-
-  describe('detail record', function () {
-
-      describe('fromName', function () {
-        testBadInputs( 'detail', this.title,
-          ['not provided', null, undefined, {}, [], 0, 123],
-          good.detail.values
-        );
-      });
-
-      describe('fromBsb', function () {
-        testBadInputs( 'detail', this.title,
-          ['not provided', null, undefined, {}, [], 0, 123382, 'abc-def', 'WBC', '452 234', '305123'],
-          good.detail.values
-        );
-      });
-
-      describe('fromAcc', function () {
-        testBadInputs( 'detail', this.title,
-          ['not provided', null, undefined, {}, [], 0, 123382, 'abc-def', 'WBC'],
-          good.detail.values
-        );
-      });
-
-      describe('toName', function () {
-        testBadInputs( 'detail', this.title,
-          ['not provided', null, undefined, {}, [], 0, 'this is way way way way way too long'],
-          good.detail.values
-        );
-      });
-
-      describe('toBsb', function () {
-        // is similar enough for now to fromBsb
-      });
-
-      describe('toAcc', function () {
-        // is similar enough for now to fromAcc
-      });
-
-      describe('toRef', function () {
-        testBadInputs( 'detail', this.title,
-          ['not provided', null, undefined, {}, [], 0, ' Start With Spc', 'has-dashes', 'this is way too long'],
-          good.detail.values
-        );
-      });
-
-      describe('indicator', function () {
-        testBadInputs( 'detail', this.title,
-          [null, {}, [], 0, 'abc-def', 'M', 'Z'],
-          good.detail.values
-        );
-      });
-
-      describe('transaction', function () {
-        testBadInputs( 'detail', this.title,
-          [0, 14, null],
-          good.detail.values
-        );
-      });
-
-      describe('amount', function () {
-        // task
-      });
-
-      describe('tax', function () {
-        // task
-      });
-
-
-  });
-
-
-  describe('total record', function () {
-
-      describe('totalNet', function () {
-        testBadInputs( 'total', this.title,
-          ['not provided', null, undefined, 'a', {}, [], 0, 999999999],
-          good.total.values
-        );
-      });
-
-      describe('totalCredit', function () {
-        testBadInputs( 'total', this.title,
-          ['not provided', null, undefined, 'a', {}, [], 0, 999999999],
-          good.total.values
-        );
-      });
-
-      describe('totalDebit', function () {
-        testBadInputs( 'total', this.title,
-          ['not provided', null, undefined, 'a', {}, [], 0, 999999999],
-          good.total.values
-        );
-      });
-
-      describe('count', function () {
-        testBadInputs( 'total', this.title,
-          ['not provided', null, undefined, 'a', {}, [], 0, -1],
-          good.total.values
-        );
-      });
-
-  });
-
-  describe('getEntry()', function () {
-
-    _.each([{}, null, undefined, ''], function (v) {
-
-      it("should throw ValidationError when value is: '" + v + "'", function () {
-        assert.throws(
-          function () {
-            getEntry('any value will do', v)
-          },
-          /ValidationError/
-        );
-      });
-
+    describe("bank", function () {
+      testBadInputs(
+        "descriptive",
+        this.title,
+        ["not provided", null, undefined, "a", "ab", "abcd", "###", 123, "123"],
+        good.descriptive.values
+      );
     });
 
+    describe("userName", function () {
+      testBadInputs(
+        "descriptive",
+        this.title,
+        ["not provided", null, undefined, ""],
+        good.descriptive.values
+      );
+    });
+
+    describe("userId", function () {
+      testBadInputs(
+        "descriptive",
+        this.title,
+        ["not provided", null, undefined, ""],
+        good.descriptive.values
+      );
+    });
+
+    describe("description", function () {
+      testBadInputs(
+        "descriptive",
+        this.title,
+        ["not provided", null, undefined, "", "very long description", 1232344],
+        good.descriptive.values
+      );
+    });
+
+    describe("date", function () {
+      testBadInputs(
+        "descriptive",
+        this.title,
+        ["a", "ab", "abcd", "###", "23/12/2014"],
+        good.descriptive.values
+      );
+    });
   });
 
+  describe("detail record", function () {
+    describe("fromName", function () {
+      testBadInputs(
+        "detail",
+        this.title,
+        ["not provided", null, undefined, {}, [], 0, 123],
+        good.detail.values
+      );
+    });
+
+    describe("fromBsb", function () {
+      testBadInputs(
+        "detail",
+        this.title,
+        [
+          "not provided",
+          null,
+          undefined,
+          {},
+          [],
+          0,
+          123382,
+          "abc-def",
+          "WBC",
+          "452 234",
+          "305123",
+        ],
+        good.detail.values
+      );
+    });
+
+    describe("fromAcc", function () {
+      testBadInputs(
+        "detail",
+        this.title,
+        ["not provided", null, undefined, {}, [], 0, 123382, "abc-def", "WBC"],
+        good.detail.values
+      );
+    });
+
+    describe("toName", function () {
+      testBadInputs(
+        "detail",
+        this.title,
+        [
+          "not provided",
+          null,
+          undefined,
+          {},
+          [],
+          0,
+          "this is way way way way way too long",
+        ],
+        good.detail.values
+      );
+    });
+
+    describe("toBsb", function () {
+      // is similar enough for now to fromBsb
+    });
+
+    describe("toAcc", function () {
+      // is similar enough for now to fromAcc
+    });
+
+    describe("toRef", function () {
+      testBadInputs(
+        "detail",
+        this.title,
+        [
+          "not provided",
+          null,
+          undefined,
+          {},
+          [],
+          0,
+          " Start With Spc",
+          "has-dashes",
+          "this is way too long",
+        ],
+        good.detail.values
+      );
+    });
+
+    describe("indicator", function () {
+      testBadInputs(
+        "detail",
+        this.title,
+        [null, {}, [], 0, "abc-def", "M", "Z"],
+        good.detail.values
+      );
+    });
+
+    describe("transaction", function () {
+      testBadInputs("detail", this.title, [0, 14, null], good.detail.values);
+    });
+
+    describe("amount", function () {
+      // task
+    });
+
+    describe("tax", function () {
+      // task
+    });
+  });
+
+  describe("total record", function () {
+    describe("totalNet", function () {
+      testBadInputs(
+        "total",
+        this.title,
+        ["not provided", null, undefined, "a", {}, [], 0, 999999999],
+        good.total.values
+      );
+    });
+
+    describe("totalCredit", function () {
+      testBadInputs(
+        "total",
+        this.title,
+        ["not provided", null, undefined, "a", {}, [], 999999999],
+        good.total.values
+      );
+    });
+
+    describe("totalDebit", function () {
+      testBadInputs(
+        "total",
+        this.title,
+        ["not provided", null, undefined, "a", {}, [], 999999999],
+        good.total.values
+      );
+    });
+
+    describe("count", function () {
+      testBadInputs(
+        "total",
+        this.title,
+        ["not provided", null, undefined, "a", {}, [], 0, -1],
+        good.total.values
+      );
+    });
+  });
+
+  describe("getEntry()", function () {
+    _.each([{}, null, undefined, ""], function (v) {
+      it(
+        "should throw ValidationError when value is: '" + v + "'",
+        function () {
+          assert.throws(function () {
+            getEntry("any value will do", v);
+          }, /ValidationError/);
+        }
+      );
+    });
+  });
 });
 
-
-describe ('#ABA', function () {
+describe("#ABA", function () {
   //mocha -g 'ABA';
 
-  var myDetailRecordValues = [{
-    fromName: 'Transport Co',
-    fromBsb: '034-069',
-    fromAcc: '3392881',
-    //indicator: ' ',
-    //transaction: ,
-    amount: 30199.00,
-    toName: 'Leryooy Jenkins',
-    toRef: 'INV 1235B',
-    toBsb: '189-213',
-    toAcc: '009872',
-    //tax: ,
-  },{
-    fromName: 'Transport Co',
-    fromBsb: '034-069',
-    fromAcc: '3392881',
-    //indicator: ' ',
-    //transaction: ,
-    amount: 1103.20,
-    toName: 'Jeffson Angus',
-    toRef: 'Reimbursement',
-    toBsb: '673-213',
-    toAcc: '10118821',
-    //tax: ,
-  }];
+  var myDetailRecordValues = [
+    {
+      fromName: "Transport Co",
+      fromBsb: "034-069",
+      fromAcc: "3392881",
+      //indicator: ' ',
+      //transaction: ,
+      amount: 30199.0,
+      toName: "Leryooy Jenkins",
+      toRef: "INV 1235B",
+      toBsb: "189-213",
+      toAcc: "009872",
+      //tax: ,
+    },
+    {
+      fromName: "Transport Co",
+      fromBsb: "034-069",
+      fromAcc: "3392881",
+      //indicator: ' ',
+      //transaction: ,
+      amount: 1103.2,
+      toName: "Jeffson Angus",
+      toRef: "Reimbursement",
+      toBsb: "673-213",
+      toAcc: "10118821",
+      //tax: ,
+    },
+  ];
 
   var myDetailRecord = {
-    fromName: 'Transport Co',
-    fromBsb: '034-069',
-    fromAcc: '3392881',
+    fromName: "Transport Co",
+    fromBsb: "034-069",
+    fromAcc: "3392881",
     //indicator: ' ',
     //transaction: ,
     amount: 220.13,
-    toName: 'Printing Co.',
-    toRef: 'PX11081',
-    toBsb: '901-109',
-    toAcc: '39810029',
+    toName: "Printing Co.",
+    toRef: "PX11081",
+    toBsb: "901-109",
+    toAcc: "39810029",
     //tax: ,
-  }
-
+  };
 
   var myDescriptiveRecord = {
     sequence: 1,
-    bank: 'WBC',
-    userName: 'LIFE CHURCH',
-    userId: '252359',
-    description: 'Payments',
+    bank: "WBC",
+    userName: "LIFE CHURCH",
+    userId: "252359",
+    description: "Payments",
     //date: '311214',
   };
 
@@ -338,9 +397,7 @@ describe ('#ABA', function () {
   var fancyNewAba = new ABA();
   fancyNewAba.addDescriptiveRecord(myDescriptiveRecord);
   fancyNewAba.addDetailRecords(myDetailRecordValues);
-  fancyNewAba.addRecord('detail', myDetailRecord);
+  fancyNewAba.addRecord("detail", myDetailRecord);
   //console.log(fancyNewAba);
   console.log(fancyNewAba.export());
-
-
 });
